55ea8e6b837b32da8cdf23e5894e049a269c2d43
hadeerkamel <hadeer_kamel@outlook.com>
2024-06-06 15:18:48 +0300

    updates

--- Sources/ArcGISMapSwift/ArcGISMapView.swift
+++ Sources/ArcGISMapSwift/ArcGISMapView.swift
@@ -19,7 +19,7 @@ public struct SearchWithGeocodeView: View {
             y: 44.986656,
             spatialReference: .wgs84
         ),
-        scale: 1e6
+        scale: 1e3
     )
     
     /// Denotes whether the map view is navigating. Used for the repeat search
@@ -51,22 +51,35 @@ public struct SearchWithGeocodeView: View {
     
     /// The view model for the sample.
     @StateObject private var model = Model()
-    public init() {
+    
+    public init(apiKey: String) {
+        ArcGISEnvironment.apiKey = APIKey(apiKey)
     }
     public var body: some View {
         MapViewReader { proxy in
             MapView(
                 map: model.map,
                 viewpoint: viewpoint,
-                graphicsOverlays: [model.searchResultsOverlay]
+                graphicsOverlays: [model.searchResultsOverlay, model.graphicsOverlay]
             )
             .onSingleTapGesture { screenPoint, tapLocation in
                 identifyScreenPoint = screenPoint
                 identifyTapLocation = tapLocation
+                dropPin(at: tapLocation)
+                Task{
+                    await getAddressFromPoint(point: tapLocation)
+                }
             }
             .onNavigatingChanged { isGeoViewNavigating = $0 }
             .onViewpointChanged(kind: .centerAndScale) {
                 queryCenter = $0.targetGeometry.extent.center
+                
+                let latitude = CoordinateFormatter.latitudeLongitudeString(from: queryCenter!, format: .decimalDegrees, decimalPlaces: 6)
+                print(latitude)
+                
+                
+                
+                //print(longitude)
             }
             .onVisibleAreaChanged { newVisibleArea in
                 // For "Repeat Search Here" behavior, use `geoViewExtent` and
@@ -80,6 +93,7 @@ public struct SearchWithGeocodeView: View {
                     .padding()
             }
             .task(id: identifyScreenPoint) {
+                
                 guard let screenPoint = identifyScreenPoint,
                       // Identifies when user taps a graphic.
                       let identifyResult = try? await proxy.identify(
@@ -87,9 +101,12 @@ public struct SearchWithGeocodeView: View {
                         screenPoint: screenPoint,
                         tolerance: 10
                       )
+                        
                 else {
                     return
                 }
+                
+                
                 // Creates a callout placement at the user tapped location.
                 calloutPlacement = identifyResult.graphics.first.flatMap { graphic in
                     CalloutPlacement.geoElement(graphic, tapLocation: identifyTapLocation)
@@ -114,8 +131,45 @@ public struct SearchWithGeocodeView: View {
                 }
                 .padding()
             }
+            
         }
     }
+    func getAddressFromPoint(point: Point) async {
+
+        
+        // Create parameters for reverse geocode
+        let params = ReverseGeocodeParameters()
+        params.maxResults = 1
+        
+        // Perform reverse geocode
+        guard let normalizedPoint = GeometryEngine.normalizeCentralMeridian(
+            of: point
+        ) as? Point else { return }
+        
+        do {
+            // Perform reverse geocode using the locator task with the point and parameters.
+            let geocodeResults = try await model.locatorTask.reverseGeocode(
+                forLocation: normalizedPoint,
+                parameters: params
+            )
+            
+            // Update the callout text using the first result from the reverse geocode.
+            print(geocodeResults.first?.attributes["LongLabel"] as? String)
+        } catch {
+            print(error)
+            
+        }
+        
+        
+    }
+    private func dropPin(at location: Point) {
+        model.markerGraphic.geometry = location
+        
+    }
+    
+    
+    
+    
 }
 
 private extension SearchWithGeocodeView {
@@ -123,14 +177,44 @@ private extension SearchWithGeocodeView {
     /// used in this view.
     class Model: ObservableObject {
         /// A map with imagery basemap.
-        let map = Map(basemapStyle: .arcGISImagery)
+        let map = Map(basemapStyle: .osmStandard)
         
         /// The graphics overlay used by the search toolkit component to display
         /// search results on the map.
         let searchResultsOverlay = GraphicsOverlay()
+        
+        let graphicsOverlay = GraphicsOverlay()
+        
+        /// The red map marker graphic used to indicate a tap location on the map.
+        let markerGraphic = {
+            // Create a symbol using the image from the project assets.
+           // guard let markerImage = UIImage(named: "RedMarker") else { return Graphic() }
+            let markerSymbol = SimpleMarkerSymbol(style: .circle, color: .red)
+            
+            // Change the symbol's offsets, so it aligns properly to a given point.
+           // markerSymbol.leaderOffsetY = markerImage.size.height / 2
+          //  markerSymbol.offsetY = markerImage.size.height / 2
+            
+            // Create a graphic with the symbol.
+            return Graphic(symbol: markerSymbol)
+        }()
+        
+        /// The locator task for reverse geocoding.
+        let locatorTask = LocatorTask(url: .geocodeServer)
+    
+        
+        init() {
+            graphicsOverlay.addGraphic(markerGraphic)
+        }
+    }
+}
+private extension URL {
+    /// A URL to a geocode server on ArcGIS Online.
+    static var geocodeServer: URL {
+        URL(string: "https://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer")!
     }
 }
 
 #Preview {
-    SearchWithGeocodeView()
+    SearchWithGeocodeView(apiKey: "AAPK02c4162a6c244595b0564d86007d14b9Wvyt7aoPDLSmphsm2gwYsNv3ov6GmtsaqObChcDJx0YGTThOj2FwZ8xQQatIp3ds")
 }
