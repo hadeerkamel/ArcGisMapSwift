6d9c85699ada6dfac1da569765c1d2041e3a0f92
hadeerkamel <hadeer_kamel@outlook.com>
2024-06-09 17:02:30 +0300

    polyline

--- Sources/ArcGISMapSwift/ArcGisMapPoints.swift
+++ Sources/ArcGISMapSwift/ArcGisMapPoints.swift
@@ -1,18 +1,90 @@
 //
-//  SwiftUIView.swift
-//  
+//  ArcGisMapPoints.swift
+//
 //
 //  Created by Hadeer on 6/9/24.
 //
 
 import SwiftUI
+import ArcGIS
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+public struct ArcGisMapPoints: View {
+    
+    @StateObject private var model = Model()
+    @State var points: [PointCoordinate]
+    
+    public init(points: [PointCoordinate]) {
+        self.points = points
+       
+    }
+    public var body: some View {
+        
+        MapView(map: model.map, graphicsOverlays: [model.graphicsOverlay])
+            .onAppear(){
+                model.points = self.points.map{return Point(x: $0.lng, y: $0.lat,spatialReference: .wgs84)}
+                model.drawPolyLine()
+            }
+    }
+    
+}
+public struct PointCoordinate{
+    let lat: Double
+    let lng: Double
+}
+private class Model: ObservableObject {
+    @Published var points: [Point] = []
+    @State var map: Map = {
+           let map = Map(basemapStyle: .osmStandard)
+           map.initialViewpoint = Viewpoint(latitude: 30.078747, longitude: 31.203802, scale: 1e3)
+           return map
+    }()
+    let graphicsOverlay: GraphicsOverlay = {
+        let graphicsOverlay = GraphicsOverlay()
+        
+        return graphicsOverlay
+
+    }()
+    init() {
+        ArcGISEnvironment.apiKey = APIKey(APIKEY)
+        drawPolyLine()
+    }
+    func drawPolyLine(){
+       
+        var perviousPoint: Point? = nil
+        for p in points {
+            if let prev = perviousPoint{
+                addLine(p1: prev, p2: p)
+                addPoint(point: prev)
+                if(p == points.last){
+                    addPoint(point: p)
+                }
+            }
+            
+            perviousPoint = p
+        }
+        
+    }
+    func addPoint(point: Point){
+        
+        guard let markerImage = ImageProvider.loadImage(named: "marker") else{return}
+        
+        let markerSymbol = PictureMarkerSymbol(image: markerImage)
+        
+        let pointGraphic = Graphic(geometry: point, symbol: markerSymbol)
+
+        graphicsOverlay.addGraphic(pointGraphic)
+    }
+    func addLine(p1:Point,p2:Point){
+        let polyline = Polyline(points: [p1,p2])
+        let polylineSymbol = SimpleLineSymbol(style: .solid, color: .green, width: 10.0)
+        let polylineGraphic = Graphic(geometry: polyline, symbol: polylineSymbol)
+
+        graphicsOverlay.addGraphic(polylineGraphic)
     }
 }
 
 #Preview {
-    SwiftUIView()
+    ArcGisMapPoints(points: [PointCoordinate(lat: 30.078747, lng: 31.203802),
+                             PointCoordinate(lat: 30.078023, lng: 31.201780),
+                             PointCoordinate(lat: 30.080108, lng: 31.201958)])
 }
