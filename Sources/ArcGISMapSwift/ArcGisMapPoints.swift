97f28035de526dc5bb46abd7d89e058db26f6426
hadeerkamel <hadeer_kamel@outlook.com>
2024-06-27 09:59:55 +0300

    d

--- Sources/ArcGISMapSwift/ArcGisMapPoints.swift
+++ Sources/ArcGISMapSwift/ArcGisMapPoints.swift
@@ -122,8 +122,8 @@ public struct ArcGisMapPoints: View {
 }
 
 public struct PointCoordinate: Equatable {
-    let lat: Double
-    let lng: Double
+    public let lat: Double
+    public let lng: Double
     public init(lat: Double, lng: Double) {
         self.lat = lat
         self.lng = lng
